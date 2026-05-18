76a6ce11e510f03a943c4702c97ae8530cb6e1c1
Hara Koki <251205146@ccmailg.meijo-u.ac.jp>
2026-05-18 14:16:25 +0900

    ミノの色を変更
    cyan -> sky-blue

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'cyan', 'gold', 'blue', 'chocolate', 'red', 'lime', 'purple'
+    'skyblue', 'gold', 'blue', 'chocolate', 'red', 'lime', 'purple'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
